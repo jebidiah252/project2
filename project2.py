ae928901679a3009b0e0cad04a508a8136bfe968
Gordon Fjeldsted <gordon.fjeldsted@gmail.com>
2021-02-08 22:10:57 -0700

    more noodling

--- project2.py
+++ project2.py
@@ -29,13 +29,25 @@ print(pearsonr[0])
 print('P Value:')
 print(pearsonr[1])
 
-print('Average Poverty Level:')
-print(povertyLevelDataSet['Populationbelowpovertylevel'].mean())
-print('Standard Deviation:')
-print(povertyLevelDataSet.std())
-
-sns.scatterplot(x='NonWhiteSum', y='Populationbelowpovertylevel', data=povertyLevelDataSet)
-sns.regplot(x='NonWhiteSum', y='Populationbelowpovertylevel', data=povertyLevelDataSet)
+# Scatter plot
+sns.lmplot(x='NonWhiteSum', y='Populationbelowpovertylevel', data=povertyLevelDataSet, hue='Zip_Code_Housing', legend=False, fit_reg=True)
+plt.xlabel('% of Non White Population')
+plt.ylabel('% of the Population below the Poverty Level')
 plt.figure()
 # %% [markdown]
 # # Turns out this thought seems true, according to the pearson correlations.
+# # But what if we were to standardize this a little bit more?
+# %%
+
+# standardized data
+povertyMean = povertyLevelDataSet['Populationbelowpovertylevel'].mean()
+povertyStd = povertyLevelDataSet['Populationbelowpovertylevel'].std()
+povertyLevelDataSet['povertyStd'] = (povertyLevelDataSet['Populationbelowpovertylevel'] - povertyMean) / povertyStd
+
+nonWhiteMean = povertyLevelDataSet['NonWhiteSum'].mean()
+nonWhiteStd = povertyLevelDataSet['NonWhiteSum'].std()
+povertyLevelDataSet['nonWhiteStd'] = (povertyLevelDataSet['NonWhiteSum'] - nonWhiteMean) / nonWhiteStd
+
+# scatterplot of standardized data
+sns.lmplot(x='povertyStd', y='nonWhiteStd', data=povertyLevelDataSet, hue='Zip_Code_Housing', legend=False)
+plt.figure()
